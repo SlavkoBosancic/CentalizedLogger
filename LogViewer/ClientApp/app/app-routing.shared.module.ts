87308b9e2dbabec677b5c69e6a588327fd8240bd
Slavko Bosancic <slavko.bosancic@gmail.com>
2017-11-25 22:09:07 +0100

    Client App - Template clean up, module and component rearrangement

---
+++ LogViewer/ClientApp/app/app-routing.shared.module.ts
@@ -0,0 +1,20 @@
+﻿import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+// Components
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+
+const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'search', component: SearchComponent },
+    { path: '**', redirectTo: '/home' }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModuleShared { }
--- LogViewer/ClientApp/app/app.shared.module.ts
+++ LogViewer/ClientApp/app/app.shared.module.ts
@@ -2,34 +2,31 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
 
-import { AppComponent } from './components/app/app.component';
+// Routing
+import { AppRoutingModuleShared } from './app-routing.shared.module';
+
+// Components
+import { AppComponent } from './components/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { CounterComponent } from './components/counter/counter.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         NavMenuComponent,
-        CounterComponent,
-        FetchDataComponent,
-        HomeComponent
+        HomeComponent,
+        SearchComponent
     ],
     imports: [
         CommonModule,
         HttpModule,
         FormsModule,
-        RouterModule.forRoot([
-            { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
-            { path: '**', redirectTo: 'home' }
-        ])
-    ]
+        AppRoutingModuleShared      // Routing module
+    ],
+    providers: []
 })
+
 export class AppModuleShared {
 }
